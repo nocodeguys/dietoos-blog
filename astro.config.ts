2192c55fb4ea201a25ee04b59096b77f86fff477
Maciej Wisniewski <nocodeguys@gmail.com>
2025-07-03 11:04:57 +0200

    Initial commit

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,14 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import tailwindcss from "@tailwindcss/vite";
+
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI
-    ? "https://astro-shadcn-ui-template.vercel.app"
-    : "http://localhost:4321",
+    ? 'https://astro-shadcn-ui-template.vercel.app'
+    : 'http://localhost:4321',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
